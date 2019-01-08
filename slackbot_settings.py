278a1c95cee5fc224892df24bb525b19d8ad8078
hohihohi <a56.tazima@gmail.com>
2019-01-08 11:37:46 +0900

    load token variable from enviroment variables

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,4 +1,10 @@
-API_TOKEN = "XXXX"
+from dotenv import load_dotenv
+from pathlib import Path
+import os
+
+env_path = Path('.') / '.env'
+load_dotenv(verbose=True, dotenv_path=env_path)
+
+API_TOKEN = os.getenv("API_TOKEN") or os.environ["API_TOKEN"]
 DEFAULT_REPLY = "Hello"
-# directory name of plugin
-PLUGINS = ['plugins']
+# PLUGINS = ['plugins'] # directory name of plugin
